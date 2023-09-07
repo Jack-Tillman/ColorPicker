a4f644eab48352004c832d3f6c8ecb2fd86a8af2
Jack-Tillman <johntillman345@gmail.com>
2023-09-07 10:12:10 -0400

    Added extra credit functionality for border

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,10 @@ import { useState } from "react";
 const App = () => {
   const [selectedColor, setSelectedColor] = useState("");
 
+  const selectColor = (colorName) => {
+    setSelectedColor(colorName);
+  }
+
   return (
     <div id="container">
       <div id="navbar">
@@ -16,30 +20,22 @@ const App = () => {
       </div>
       <div id="colors-list">
         {/*  added id to select each color circle later */}
-        <Color color="red" id="red" setSelectedColor={setSelectedColor} />
-        <Color color="blue" id="blue" setSelectedColor={setSelectedColor} />
-        <Color color="green" id="green" setSelectedColor={setSelectedColor} />
+        <Color color="red" selectColor={selectColor} selectedColor={selectedColor} setSelectedColor={setSelectedColor} />
+        <Color color="blue" selectColor={selectColor} selectedColor={selectedColor} setSelectedColor={setSelectedColor} />
+        <Color color="green" selectColor={selectColor} selectedColor={selectedColor} setSelectedColor={setSelectedColor} />
       </div>
     </div>
   );
 };
 
-const Color = ({ color, setSelectedColor }) => {
+const Color = ({ color, setSelectedColor, selectedColor }) => {
+  const isSelected = selectedColor === color ? "selected" : "";
+
   return (
     <div
-      className={color}
+      className={`${color} ${isSelected}`}
       onClick={() => {
         setSelectedColor(color);
-        {
-          /* select whichever circle is selected and add .selected class to it*/
-        }
-        const circle = document.querySelector(`.${color}`);
-        circle.classList.add("selected");
-
-        {
-          /*  wanted a better way to remove the selected border around a circle after user clicks on another circle, but this is my best try at it  */
-        }
-        setTimeout(() => circle.classList.remove("selected"), 2000);
       }}
     />
   );
